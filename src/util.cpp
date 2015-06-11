29d72712e515edd16e66e97d46116119feb598b1
oVooVo <pascal.bies.42@gmail.com>
2015-06-11 19:29:42 +0200

    replaced initializer list by c98-standard equivalent from util. There are probably other initializer lists etc.

--- src/util.cpp
+++ src/util.cpp
@@ -9,12 +9,12 @@ QString replaceDiacritics(QString string)
 {
     // see http://stackoverflow.com/questions/14009522/how-to-remove-accents-diacritic-marks-from-a-string-in-qt
     const QString DIACRITIC_LETTERS = QString::fromUtf8("ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ");
-    const QStringList NON_DIACRITIC_LETTERS = QStringList({ "S", "OE", "Z", "s", "oe", "z", "Y", "Y", "u", "A", "A", "A",
-                                                            "A", "A", "A", "AE", "C", "E", "E", "E", "E", "I", "I", "I",
-                                                            "I", "D", "N", "O", "O", "O", "O", "O", "O", "U", "U", "U",
-                                                            "U", "Y", "s", "a", "a", "a", "a", "ae", "a", "ae", "c", "e", "e",
-                                                            "e", "e", "i", "i", "i", "i", "o", "n", "o", "o", "o", "o", "oe",
-                                                            "o", "u", "u", "u", "ue", "y", "y" });
+    const QStringList NON_DIACRITIC_LETTERS = QStringList() <<  "S" << "OE" << "Z" << "s" << "oe" << "z" << "Y" << "Y" << "u" << "A" << "A" << "A" <<
+                                                                "A" << "A" << "A" << "AE" << "C" << "E" << "E" << "E" << "E" << "I" << "I" << "I" <<
+                                                                "I" << "D" << "N" << "O" << "O" << "O" << "O" << "O" << "O" << "U" << "U" << "U" <<
+                                                                "U" << "Y" << "s" << "a" << "a" << "a" << "a" << "ae" << "a" << "ae" << "c" << "e" << "e" <<
+                                                                "e" << "e" << "i" << "i" << "i" << "i" << "o" << "n" << "o" << "o" << "o" << "o" << "oe" <<
+                                                                "o" << "u" << "u" << "u" << "ue" << "y" << "y";
 
     for (int i = 0; i < DIACRITIC_LETTERS.length(); ++i)
     {
