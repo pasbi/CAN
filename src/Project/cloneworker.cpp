459640b86062628f19fb5644ea7528cbe4da0bc2
oVooVo <pascal.bies.42@gmail.com>
2016-04-24 18:58:09 +0200

    removed qDebug

--- src/Project/cloneworker.cpp
+++ src/Project/cloneworker.cpp
@@ -15,10 +15,6 @@ CloneWorker::CloneWorker(git_repository*& repository, const QString& url, const 
 
 void CloneWorker::run()
 {
-    qDebug() << "clone!";
-    qDebug() << "url = " << m_url;
-    qDebug() << "nam = " << ((GitHandler::Payload*) m_options->fetch_opts.callbacks.payload)->username;
-    qDebug() << "pas = " << ((GitHandler::Payload*) m_options->fetch_opts.callbacks.payload)->password;
     int error = git_clone( &m_repository, CSTR(m_url), CSTR(m_path), m_options);
     m_error = (error != 0);
     m_isFinished = true;
